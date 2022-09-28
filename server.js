5a174377c1a1266a478719c376ba268064bc97b2
yiladien@hotmail.com <yiladien@hotmail.com>
2022-09-27 22:34:51 -0500

    added id and 404

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const PORT = process.env.PORT || 3001;
 // express
 const app = express();
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
+  return result;
+}
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   // Note that we save the animalsArray as filteredResults here:
@@ -60,6 +65,15 @@ app.get("/api/animals", (req, res) => {
     results = filterByQuery(req.query, results);
   }
   res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(PORT, () => {
